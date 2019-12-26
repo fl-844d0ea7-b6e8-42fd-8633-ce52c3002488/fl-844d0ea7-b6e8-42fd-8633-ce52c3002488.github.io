cd9a46691045597aee4fb5730fcbcf9aba7818e9
Darren Ho <chdarren.ho@gmail.com>
2019-12-26 15:04:24 +0000

    gonna try to buld the vault as a static file hosted html

---
+++ flashcards-vault/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+const production = procqess.env.NODE_ENV === 'production'
+
+const fileNameTemplate = ext => (production ?
+  `[name].[hash].min.${ext}` :
+  `[name].${ext}`)
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env"]
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(
+      [
+        'NODE_ENV',
+        'DB_CONNECTION'
+      ]
+    ),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/",
+      filename: 'bundle.js'
+    },
+    devServer: {
+      port: process.env.PORT,
+      host: '0.0.0.0',
+      compress: true,
+    },
+};
