082fc32d8b0507a80b0417186d246267bb6c5dbf
Darren Ho <chdarren.ho@gmail.com>
2020-01-03 11:15:49 +0000

    Removing env preset

--- flashcards-vault/webpack.config.js
+++ flashcards-vault/webpack.config.js
@@ -12,9 +12,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: {
-          presets: ["@babel/env"]
-        }
       },
     ]
   },
