3b4163a5c7d4358a1cddb583fa90875fc6603ba2
Darren Ho <chdarren.ho@gmail.com>
2020-04-15 12:26:05 +0100

    #19: Removing console log of api hostname

--- flashcards-generator/src/client/connectors/apigateway.js
+++ flashcards-generator/src/client/connectors/apigateway.js
@@ -6,8 +6,6 @@ export const getTopics = async () => {
   console.log('Making GET request to Flashcards Vault get TOPICS');
 
   try {
-    console.log(`Making request to: ${API_HOSTNAME}/api/listTopics`)
-
     const resp = await axios.get(`${API_HOSTNAME}/api/listTopics`);
 
     console.log('Request was successful, returning results');
