e4dae1cbd248c4ca7ae8130a26b400edb09c71a7
Darren <chdarren.ho@gmail.com>
2020-08-28 11:52:33 +0100

    Fixing build issue

--- flashcards-generator/src/client/viewFlashcards/FlashcardViewer.jsx
+++ flashcards-generator/src/client/viewFlashcards/FlashcardViewer.jsx
@@ -6,7 +6,7 @@ import FormInput from '../common/FormInput'
 import FormAlert from '../common/FormAlert'
 import TopicSelect from '../common/TopicSelect'
 import Button from 'react-bootstrap/Button'
-import { getFlashcards, deleteFlashcard } from '../connectors/serverData'
+import { getFlashcards } from '../connectors/apigateway'
 
 const FlashcardsViewer = () => {
 
